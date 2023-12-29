17d671cfe87295d0781eed1c5948f2c1f59d865f
BartJesper <bartjesperw@gmail.com>
2023-12-29 11:45:34 +0100

    Small change

--- src/Compression.java
+++ src/Compression.java
@@ -2,23 +2,21 @@ public class Compression {
 
     public static String compress(String input) {
         if(input == null) return null;
-
-        char[] charArray = input.toCharArray();
         StringBuilder result = new StringBuilder();
 
         int count = 0;
-        for(int i = 0; i < charArray.length; i++) {
+        for(int i = 0; i < input.length(); i++) {
             if(i == 0) {
                 count++;
             } else {
-                if(charArray[i] == charArray[i-1]) {
+                if(input.charAt(i) == input.charAt(i-1)) {
                     count++;
                 } else {
-                    result.append(charArray[i - 1]).append("±").append(count);
+                    result.append(input.charAt(i-1)).append("±").append(count);
                     count = 1;
                 }
-                if(i == charArray.length - 1) {
-                    result.append(charArray[i]).append("±").append(count);
+                if(i == input.length() - 1) {
+                    result.append(input.charAt(i)).append("±").append(count);
                 }
             }
         }
@@ -27,27 +25,25 @@ public class Compression {
     }
 
     public static String decompress(String input) {
-        if(input == null) throw new IllegalArgumentException("Input isn't compressed correctly.");
-
-        char[] charArray = input.toCharArray();
+        if(input == null) return null;
         StringBuilder result = new StringBuilder();
 
         char character;
         StringBuilder digitString;
-        for(int i = 0; i < charArray.length; i++) {
-            character = charArray[i];
-            if(charArray[i+1] != 177) throw new IllegalArgumentException("Input isn't compressed correctly.");
+        for(int i = 0; i < input.length(); i++) {
+            character = input.charAt(i);
+            if(input.charAt(i+1) != 177) throw new IllegalArgumentException("Input isn't compressed correctly.");
 
             digitString = new StringBuilder();
 
-            for(int j = i + 2; j < charArray.length; j++) {
-                if(Character.isDigit(charArray[j])) {
-                    if(j + 1 >= charArray.length) {
-                        digitString.append(charArray[j]);
+            for(int j = i + 2; j < input.length(); j++) {
+                if(Character.isDigit(input.charAt(j))) {
+                    if(j + 1 >= input.length()) {
+                        digitString.append(input.charAt(j));
                         i = j + 1;
                         break;
-                    } else if(charArray[j+1] != 177 || charArray[j+2] == 177) {
-                        digitString.append(charArray[j]);
+                    } else if(input.charAt(j+1) != 177 || input.charAt(j+2) == 177) {
+                        digitString.append(input.charAt(j));
                     } else {
                         i = j - 1;
                         break;
